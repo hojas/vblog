850f12d49d35dc5f1622ae36e6ed20d0039afe49
hojas <cyxhuan9@163.com>
2015-10-30 08:21:51 +0000

    add mongoose

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 var koa = require('koa');
+var mongoose = require('mongoose');
 var app = koa();
+
+mongoose.connect('mongodb://localhost:27017/kblog');
+mongoose.connection.on('error', console.error.bind(console, '连接数据库失败'));
 
 app.use(function *() {
     this.body = 'hello';
