972470812ea0230dd52006b0f98a8dbd6b95474b
hojas <cyxhuan9@163.com>
2015-11-21 02:12:39 +0000

    run webpack with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,32 @@
 'use strict';
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+var util = require('gulp-util');
+var compass = require('gulp-compass');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config');
 
+gulp.task('compass', function() {
+    gulp.src('./app/public/css/sass/*.scss')
+        pipe(compass({
+            config_file: './app/public/css/config.rb',
+            css: 'stylesheets',
+            sass: 'sass',
+        }))
+        .pipe(gulp.dest('./app/public/css'));
+});
+
 gulp.task('webpack', function(callback) {
     webpack(webpackConfig, function(err, stats) {
-        if (err) throw new gutil.PluginError('webpack', err);
-        gutil.log('[webpack]', stats.toString({
+        if (err) throw new util.PluginError('webpack', err);
+        util.log('[webpack]', stats.toString({
             'msg': 'done',
         }));
         callback && callback();
     });
+});
+
+gulp.task('default', function() {
+    gulp.run('webpack');
 });
 
