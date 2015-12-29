d3e0cc1d171e706477f282d06e8d984f9d9eccce
hojas <cyxhuan9@163.com>
2015-12-29 07:13:56 +0000

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
-var util = require('gulp-util');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var compass = require('gulp-compass');
-var webpack = require('webpack');
+var path          = require('path');
+
+var gulp          = require('gulp');
+var util          = require('gulp-util');
+var rename        = require('gulp-rename');
+var uglify        = require('gulp-uglify');
+
+var minifyCss     = require('gulp-minify-css');
+var compass       = require('gulp-compass');
+var autoprefixer  = require('gulp-autoprefixer');
+
+var webpack       = require('webpack');
 var webpackConfig = require('./webpack.config');
 
 
@@ -14,16 +19,22 @@ var webpackConfig = require('./webpack.config');
 gulp.task('compass', function() {
     gulp.src('./app/public/css/sass/**/*.scss')
         .pipe(compass({
-            config_file: './app/public/css/config.rb',
-            css: 'app/public/css/stylesheets',
-            sass: 'app/public/css/sass'
-        }));
+            project: path.join(__dirname, 'app/public/css'),
+            css: 'stylesheets',
+            sass: 'sass',
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 version'],
+        }))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('stylesheets'));
 });
 
 // run webpack
 gulp.task('webpack', function() {
     webpack(webpackConfig, function(err, stats) {
         if (err) throw new util.PluginError('webpack', err);
+        util.log('[webpack]', stats.toString({}));
     });
 });
 
