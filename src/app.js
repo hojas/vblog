fbfc917548d783a6d1e98d83367658b7d31208e6
hojas <cyxhuan9@163.com>
2017-11-03 17:08:33 +0800

    remove iview

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
-//import 'iview/dist/styles/iview.css'
-
 import 'babel-polyfill'
 import Vue from 'vue'
-import iView from 'iview'
 import VueHead from 'vue-head'
 
 import App from './components/base/app.vue'
 import router from './router'
 import store from './store'
 
-Vue.use(iView)
 Vue.use(VueHead)
 
 new Vue({
